dasnixon/ember.js

commit 7afdc4108ba0ee7d239417a1ac97f8aab44016f7

Run Ember in node
This commit removes any strict dependencies on browser-only APIs.
Anything user agent specific (such as window, document, etc.) are
abstracted out into a new `environment` object.

Currently we feature detect whether a DOM implementation is available
and only enable certain behavior if so. The intent of the feature
detection is to determine if a DOM implementation is available, not
whether we’re running in the browser or in node. This makes it possible
to run Ember in a node environment with a “fake” DOM.

This commit also introduces some refactoring to the location APIs, such
that hardcoded document/history/navigator dependencies are reduced.

Note that some APIs are guarded with `environment.hasDOM` checks.
Code inside those guards can freely use DOM APIs, but some of those
APIs are needed for full server-side usage of Ember. This is a first
step that is designed to explicitly separate out code with DOM
dependencies from isomorphic code. Further PRs will implement the
Renderer and Location APIs for non-DOM environments.

packages/ember-routing/lib/location/history_location.js
@@ -12,7 +12,6 @@ import jQuery from "ember-views/system/jquery";
 */
 
 var popstateFired = false;
-var supportsHistoryState = window.history && 'state' in window.history;
 
 /**
   Ember.HistoryLocation implements the location API using the browser's
@@ -82,7 +81,7 @@ export default EmberObject.extend({
     @param path {String}
   */
   setURL: function(path) {
-    var state = this.getState();
+    var state = this._historyState;
     path = this.formatURL(path);
 
     if (!state || state.path !== path) {
@@ -99,26 +98,12 @@ export default EmberObject.extend({
     @param path {String}
   */
   replaceURL: function(path) {
-    var state = this.getState();
+    var state = this._historyState;
     path = this.formatURL(path);
 
     if (!state || state.path !== path) {
       this.replaceState(path);
     }
-  },
-
-  /**
-   Get the current `history.state`. Checks for if a polyfill is
-   required and if so fetches this._historyState. The state returned
-   from getState may be null if an iframe has changed a window's
-   history.
-
-   @private
-   @method getState
-   @return state {Object}
-  */
-  getState: function() {
-    return supportsHistoryState ? get(this, 'history').state : this._historyState;
   },
 
   /**
@@ -131,12 +116,9 @@ export default EmberObject.extend({
   pushState: function(path) {
     var state = { path: path };
 
-    get(this, 'history').pushState(state, null, path);
+    get(this, 'history').pushState(null, null, path);
 
-    // store state if browser doesn't support `history.state`
-    if (!supportsHistoryState) {
-      this._historyState = state;
-    }
+    this._historyState = state;
 
     // used for webkit workaround
     this._previousURL = this.getURL();
@@ -151,12 +133,9 @@ export default EmberObject.extend({
   */
   replaceState: function(path) {
     var state = { path: path };
-    get(this, 'history').replaceState(state, null, path);
+    get(this, 'history').replaceState(null, null, path);
 
-    // store state if browser doesn't support `history.state`
-    if (!supportsHistoryState) {
-      this._historyState = state;
-    }
+    this._historyState = state;
 
     // used for webkit workaround
     this._previousURL = this.getURL();

packages/ember-application/lib/system/application.js
@@ -31,16 +31,10 @@ import AutoLocation from "ember-routing/location/auto_location";
 import NoneLocation from "ember-routing/location/none_location";
 import BucketCache from "ember-routing/system/cache";
 
-// this is technically incorrect (per @wycats)
-// it should work properly with:
-// `import ContainerDebugAdapter from 'ember-extension-support/container_debug_adapter';` but
-// es6-module-transpiler 0.4.0 eagerly grabs the module (which is undefined)
-
 import ContainerDebugAdapter from "ember-extension-support/container_debug_adapter";
 
-import {
-  K
-} from 'ember-metal/core';
+import { K } from 'ember-metal/core';
+import environment from 'ember-metal/environment';
 
 function props(obj) {
   var properties = [];
@@ -276,7 +270,10 @@ var Application = Namespace.extend(DeferredMixin, {
 
     if (!librariesRegistered) {
       librariesRegistered = true;
-      Ember.libraries.registerCoreLibrary('jQuery', jQuery().jquery);
+
+      if (environment.hasDOM) {
+        Ember.libraries.registerCoreLibrary('jQuery', jQuery().jquery);
+      }
     }
 
     if (Ember.LOG_VERSION) {
@@ -695,7 +692,10 @@ var Application = Namespace.extend(DeferredMixin, {
     @method didBecomeReady
   */
   didBecomeReady: function() {
-    this.setupEventDispatcher();
+    if (environment.hasDOM) {
+      this.setupEventDispatcher();
+    }
+
     this.ready(); // user hook
     this.startRouting();
 

packages/ember-debug/lib/main.js
@@ -4,6 +4,8 @@ import Ember from "ember-metal/core";
 import EmberError from "ember-metal/error";
 import Logger from "ember-metal/logger";
 
+import environment from "ember-metal/environment";
+
 /**
 Ember Debug
 
@@ -218,7 +220,7 @@ if (!Ember.testing) {
 
   // Inform the developer about the Ember Inspector if not installed.
   var isFirefox = typeof InstallTrigger !== 'undefined';
-  var isChrome = !!window.chrome && !window.opera;
+  var isChrome = environment.isChrome;
 
   if (typeof window !== 'undefined' && (isFirefox || isChrome) && window.addEventListener) {
     window.addEventListener("load", function() {

packages/ember-htmlbars/lib/main.js
@@ -47,6 +47,8 @@ import { registerASTPlugin } from "ember-htmlbars/plugins";
 import TransformEachInToHash from "ember-htmlbars/plugins/transform-each-in-to-hash";
 import TransformWithAsToHash from "ember-htmlbars/plugins/transform-with-as-to-hash";
 
+import environment from "ember-metal/environment";
+
 // importing adds template bootstrapping
 // initializer to enable embedded templates
 import "ember-htmlbars/system/bootstrap";
@@ -95,8 +97,10 @@ if (Ember.FEATURES.isEnabled('ember-htmlbars')) {
 
 }
 
+var domHelper = environment.hasDOM ? new DOMHelper() : null;
+
 export var defaultEnv = {
-  dom: new DOMHelper(),
+  dom: domHelper,
 
   hooks: {
     get: get,

packages/ember-htmlbars/lib/system/bootstrap.js
@@ -11,6 +11,7 @@ import jQuery from "ember-views/system/jquery";
 import EmberError from "ember-metal/error";
 import { onLoad } from "ember-runtime/system/lazy_load";
 import htmlbarsCompile from "ember-htmlbars/system/compile";
+import environment from "ember-metal/environment";
 
 /**
 @module ember
@@ -87,7 +88,7 @@ onLoad('Ember.Application', function(Application) {
 
   Application.initializer({
     name: 'domTemplates',
-    initialize: _bootstrap
+    initialize: environment.hasDOM ? _bootstrap : function() { }
   });
 
   Application.initializer({

packages/ember-metal-views/lib/renderer.js
@@ -1,4 +1,7 @@
 import { DOMHelper } from "morph";
+import environment from "ember-metal/environment";
+
+var domHelper = environment.hasDOM ? new DOMHelper() : null;
 
 function Renderer() {
   this._uuid = 0;
@@ -7,7 +10,7 @@ function Renderer() {
   this._parents = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
   this._elements = new Array(17);
   this._inserts = {};
-  this._dom = new DOMHelper();
+  this._dom = domHelper;
 }
 
 function Renderer_renderTree(_view, _parentView, _insertAt) {

packages/ember-metal/lib/environment.js
@@ -1,0 +1,41 @@
+
+/*
+  Ember can run in many different environments, including environments like
+  Node.js where the DOM is unavailable. This object serves as an abstraction
+  over the browser features that Ember relies on, so that code does not
+  explode when trying to boot in an environment that doesn't have them.
+
+  This is a private abstraction. In the future, we hope that other
+  abstractions (like `Location`, `Renderer`, `dom-helper`) can fully abstract
+  over the differences in environment.
+*/
+var environment;
+
+// This code attempts to automatically detect an environment with DOM
+// by searching for window and document.createElement. An environment
+// with DOM may disable the DOM functionality of Ember explicitly by
+// defining a `disableBrowserEnvironment` ENV.
+var hasDOM = typeof window !== 'undefined' &&
+             typeof document !== 'undefined' &&
+             typeof document.createElement === 'function' &&
+             !Ember.ENV.disableBrowserEnvironment;
+
+if (hasDOM) {
+  environment = {
+    hasDOM: true,
+    isChrome: !!window.chrome && !window.opera,
+    location: window.location,
+    history: window.history,
+    userAgent: window.navigator.userAgent
+  };
+} else {
+  environment = {
+    hasDOM: false,
+    isChrome: false,
+    location: null,
+    history: null,
+    userAgent: "Lynx (textmode)"
+  };
+}
+
+export default environment;

packages/ember-routing/lib/location/api.js
@@ -1,4 +1,5 @@
 import Ember from "ember-metal/core"; // deprecate, assert
+import environment from "ember-metal/environment";
 
 /**
 @module ember
@@ -178,7 +179,7 @@ export default {
   },
 
   implementations: {},
-  _location: window.location,
+  _location: environment.location,
 
   /**
     Returns the current `location.hash` by parsing location.href since browsers

commit 759800a1447d5632fd74d0ef5fd3bcd2d9d78c6a

Correctly update URL after browser popstate
This commit partially reverts 7afdc4108ba0ee7d239417a1ac97f8aab44016f7,
which introduced a regression in the way HistoryLocation works.

Prior to the above-mentioned commit, we would always consult the browser’s state stack when determining what the current path was.

The one exception to this was in cases where HistoryLocation was used
in browsers that do not support the history API. In that case, the in line comments described a fallback “polyfill” for tracking state.

Our rationale behind the refactor was that, if we have a fallback path
anyway, we should just always use that instead of having two paths
through the system.

In reality, the fallback path was a lower-fidelity implementation.
In particular, there was no analog to the user hitting the back button,
which generates a popstate event.

This commit reverts to the old behavior, so if the browser supports
history state, we always check its current state when setting the URL.
This makes sure that the path is correctly determined even if the user
hit the back button, rather than using the last cached set value.

Fixes #10801

packages/ember-routing/lib/location/history_location.js
@@ -27,6 +27,7 @@ export default EmberObject.extend({
   init() {
     set(this, 'location', get(this, 'location') || window.location);
     set(this, 'baseURL', jQuery('base').attr('href') || '');
+
   },
 
   /**
@@ -36,7 +37,13 @@ export default EmberObject.extend({
     @method initState
   */
   initState() {
-    set(this, 'history', get(this, 'history') || window.history);
+    var history = get(this, 'history') || window.history;
+    set(this, 'history', history);
+
+    if (history && 'state' in history) {
+      this.supportsHistory = true;
+    }
+
     this.replaceState(this.formatURL(this.getURL()));
   },
 
@@ -81,7 +88,7 @@ export default EmberObject.extend({
     @param path {String}
   */
   setURL(path) {
-    var state = this._historyState;
+    var state = this.getState();
     path = this.formatURL(path);
 
     if (!state || state.path !== path) {
@@ -98,12 +105,30 @@ export default EmberObject.extend({
     @param path {String}
   */
   replaceURL(path) {
-    var state = this._historyState;
+    var state = this.getState();
     path = this.formatURL(path);
 
     if (!state || state.path !== path) {
       this.replaceState(path);
     }
+  },
+
+  /**
+    Get the current `history.state`. Checks for if a polyfill is
+    required and if so fetches this._historyState. The state returned
+    from getState may be null if an iframe has changed a window's
+    history.
+
+    @private
+    @method getState
+    @return state {Object}
+  */
+  getState() {
+    if (this.supportsHistory) {
+      return get(this, 'history').state;
+    }
+
+    return this._historyState;
   },
 
   /**
@@ -116,7 +141,7 @@ export default EmberObject.extend({
   pushState(path) {
     var state = { path: path };
 
-    get(this, 'history').pushState(null, null, path);
+    get(this, 'history').pushState(state, null, path);
 
     this._historyState = state;
 
@@ -133,7 +158,7 @@ export default EmberObject.extend({
   */
   replaceState(path) {
     var state = { path: path };
-    get(this, 'history').replaceState(null, null, path);
+    get(this, 'history').replaceState(state, null, path);
 
     this._historyState = state;
 